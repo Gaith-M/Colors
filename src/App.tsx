fead9b0e98ef991365f10b729c3849f41c594581
Gaith <mhdgaith94@gmail.com>
2023-04-17 22:52:22 +0300

    created folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import { Navbar } from "./components"
 import { motion } from "framer-motion"
 
 function App() {
   return (
-    <div>
-      <motion.h1 
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        transition={{duration: 1.5}}
-        className='font-bold text-7xl text-center text-violet-600'>
-          React - Vite - Tailwindcss
-        </motion.h1>
-    </div>
+    <>
+      <Navbar/>
+      <div>
+        <motion.h1 
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          transition={{duration: 1.5}}
+          className='font-bold text-7xl text-center text-violet-600 mt-[40px]'>
+            React - Vite - Tailwindcss
+          </motion.h1>
+      </div>
+    </>
   )
 }
 
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <div className="h-[40px] bg-slate-100 drop-shadow-sm absolute top-0 left-0 right-0
+    flex justify-between items-center">
+        <span className="bg-green-300">Logo</span>
+
+        <nav>
+            <span>Link</span>
+            <span>Link</span>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
